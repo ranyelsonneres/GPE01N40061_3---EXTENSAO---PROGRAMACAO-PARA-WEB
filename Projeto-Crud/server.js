fa02f529eb36e8f8a47eb1f63324447ee41ad6c0
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-07 17:19:24 -0300

    Update server.js

--- Projeto-Crud/server.js
+++ Projeto-Crud/server.js
@@ -1,7 +1,7 @@
 const express = require('express'); //importar o framework
-const app = express(); //instancia
-const port = 3000;
-const path = require('path');
+const app = express(); //instancia do express
+const port = 3000; //porta do servidor
+const path = require('path'); //permitir trabalhar com o caminho de arquivos estáticos
 
 //estabelecer onde estão os arquivos do front
 app.use(express.static('public'));
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 //instanciar o banco de dados
 const db = require('./db');
 
-//rota
+//rota padrão para puxar o arquivo do front-end
 app.get('/', (req, res) =>{
     //res.send("front-funcionando");
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -18,9 +18,10 @@ app.get('/', (req, res) =>{
 //linkar com a API.JS
 const apiRoutes = require('./routes/api');
 
-//middleware
+//middleware para interpretar o corpo das requisições que chegam no formato JSON
 app.use(express.json());
 
+//define o caminho base para as rotas importadas para o módulo "apiRoutes"
 app.use('/api/users/', apiRoutes);
 
 //porta do servidor
