9ff6c821c86a51076820082d3dab5dee626a2c80
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-11 16:27:05 -0300

    Update app.js

--- Projeto-Crud/public/app.js
+++ Projeto-Crud/public/app.js
@@ -39,10 +39,10 @@ function carregarUsuarios(){
         userList.innerHTML = '';
         data.forEach(user => {
             const li = document.createElement('li');
-            li.innerHTML = `Nome: ${user.nome} - Email: ${user.email}
-            <button onclick="excluirUsuario(${user.id})">Excluir</button>
-            `
-            userList.append(li);
+            li.innerHTML = `${user.nome} (${user.email}) 
+                <button onclick="atualizarUsuario(${user.id})">Editar</button>
+                <button onclick="excluirUsuario(${user.id})">Excluir</button>`;
+                userList.appendChild(li);
         });
     });
 };
@@ -50,8 +50,19 @@ function carregarUsuarios(){
 
 
 
-//função para editar
-
+//função para editar usuários
+function atualizarUsuario(id) {
+    const nome = prompt('Digite o novo nome:');
+    const email = prompt('Digite o novo email:');
+    fetch(`/api/users/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nome, email })
+    })
+    .then(() => {
+        carregarUsuarios();
+    })
+}
 
 //função para excluir
 function excluirUsuario(id){
